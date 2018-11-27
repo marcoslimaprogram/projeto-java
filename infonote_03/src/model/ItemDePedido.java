8cfb821c4949350441c821a491b9052e26f25589
marcoslimaprogram <marcos_lima_rj@hotmail.com>
2018-11-27 21:04:43 -0200

    Atualizado

---
+++ infonote_03/src/model/ItemDePedido.java
@@ -0,0 +1,50 @@
+package model;
+
+public class ItemDePedido {
+
+	private int qtde;
+	private double subtotal;
+
+	public void mostrar() {
+
+		// Mostrar usuário
+		System.out.println("\n\nItemDePedido: ");
+		System.out.println("qtde" + this.qtde);
+		System.out.println("subtotal" + this.subtotal + "\n" );
+
+	}
+
+	public ItemDePedido(int qtde, double subtotal) {
+		super();
+		this.qtde = qtde;
+		this.subtotal = subtotal;
+	}
+
+	public ItemDePedido() {
+		super();
+	}
+
+	public int getQtde() {
+		return qtde;
+	}
+
+	public void setQtde(int qtde) {
+		this.qtde = qtde;
+	}
+
+	public double getSubtotal() {
+		return subtotal;
+	}
+
+	public void setSubtotal(double subtotal) {
+		this.subtotal = subtotal;
+	}
+
+	@Override
+	public String toString() {
+		return "ItenDePedido [qtde=" + qtde + ", subtotal=" + subtotal + ", getQtde()=" + getQtde() + ", getSubtotal()="
+				+ getSubtotal() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+
+}
